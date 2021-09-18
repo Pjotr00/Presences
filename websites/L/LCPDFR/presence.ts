0174dd0d2263e8e04480d9a2490dfcf4cd884192
Pjotr00 <DevPjotr@tutanota.com>
2021-09-18 20:46:32 +0200

    Rewriting the presence

--- websites/L/LCPDFR/presence.ts
+++ websites/L/LCPDFR/presence.ts
@@ -1,103 +1,30 @@
 const presence = new Presence({
-    clientId: "768903129194037260"
+  clientId: "768903129194037260"
 });
 
 presence.on("UpdateData", () => {
-    const doc = document.location,
+  const doc = document.location,
     path = doc.pathname,
     route = path.split("/"),
     browsingStamp = Math.floor(Date.now() / 1000),
     presenceData: PresenceData = {
-      largeImageKey: "lcpdfr",
+      largeImageKey: "lawco",
       startTimestamp: browsingStamp
     };
+  
+  if (path == "/") {
+    presenceData.details = "Browsing the homepage"
+  };
 
-    if (path == "/") {
-        presenceData.details = "Browsing the homepage";
-    }
+  switch (route[1]) {  
+    default:
+      break;
+  }
 
-    switch (route[1]) {
-        // News app
-        case "news":
-            if (route[3]) {
-                presenceData.details = "Reading a news article";
-                presenceData.state = document.querySelector("h1.ipsType_pageTitle").textContent;
-            }
-            else {
-                presenceData.details = "Browsing the news";
-            }
-            break;
-
-        // Forums
-        case "forums":
-            switch (route[2]) {
-                case "forum":
-                    presenceData.details = "Viewing a forum";
-                    presenceData.state = document.querySelector(".ipsType_pageTitle").textContent.trim();
-                    break;
-                case "topic":
-                    presenceData.details = "Reading a forum topic";
-                    presenceData.state = document.querySelector("h1.ipsType_pageTitle").textContent.trim();
-                    break;
-                default:
-                    presenceData.details = "Browsing the forums";
-                    break;
-            }
-            break;
-        
-        case "gallery":
-            switch (route[2]) {
-                case "category":
-                    presenceData.details = "Viewing a gallery category";
-                    presenceData.state = document.querySelector(".ipsType_pageTitle").textContent.trim();
-                    break;
-                
-                case "album":
-                    presenceData.details = "Viewing an album";
-                    presenceData.state = `"${document.querySelector("h1.ipsType_pageTitle").textContent.trim()}" by ${document.querySelector("span.ipsType_normal:nth-child(3) > a:nth-child(1)").textContent.trim()}`;
-                    break;
-                
-                case "image":
-                    presenceData.details = "Viewing a photo";
-                    presenceData.state = `"${document.querySelector(".cGalleryImageTitle > h1:nth-child(1)").textContent.trim()}" by ${document.querySelector("p.ipsType_sectionHead").textContent.trim()}`;
-                    break;
-            
-                default:
-                    presenceData.details = "Viewing the gallary";
-                    break;
-            }
-            break;
-        
-        case "guideline-hub":
-            if (route[2]) {
-                presenceData.details = "Reading a guideline page";
-                presenceData.state = document.querySelector(".ipsType_pageTitle").textContent.trim();
-            }
-            else {
-                presenceData.details = "Browsing the guidelines";
-            }
-            break;
-        
-        case "leaderboard":
-            presenceData.details = "Viewing the leaderboard";
-            break;
-        
-        case "pastleaders":
-            presenceData.details = "Viewing the past leaders";
-            break;
-        
-        case "topmembers":
-            presenceData.details = "Viewing the top members";
-            break;
-
-        default:
-            break;
-    }
-
-    if (presenceData.details == null) {
-        presence.setTrayTitle();
-        presence.setActivity();
-    } else {
-        presence.setActivity(presenceData);
-    }
+  if (presenceData.details == null) {
+    presence.setTrayTitle();
+    presence.setActivity();
+  } else {
+    presence.setActivity(presenceData)
+  }
 });
